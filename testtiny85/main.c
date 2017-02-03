f6ad27a8ff7d6fe1e150efff66eb5e8cc0cc47d7
mizo <mizo@MIZO-PC>
2017-02-03 16:36:05 +0100

    enable ADC

--- testtiny85/main.c
+++ testtiny85/main.c
@@ -6,22 +6,31 @@
  */
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdint.h>
 
 int main(void)
 {
 	DDRB |= (1<<PB0); // output on PB0
 	TCCR0A = (1 << COM0A1) | (1 << WGM00); //Phase-correct PWM
 	TCCR0B = (1 << CS01); // clock with prescaler CLK/8
+
+	ADCSRA |= (1 << ADEN); //ADC enable
+	uint8_t PWM_val = 0u;
+	uint8_t dir = 0u;
 	while(1)
 	{
-		_delay_ms(200);
-		OCR0A = 0x10; // set analog value
-		_delay_ms(200);
-		OCR0A = 0x30;
-		_delay_ms(200);
-		OCR0A = 0x50;
-		_delay_ms(200);
-		OCR0A = 0xA0;
+		if ((0u == PWM_val) || (255u == PWM_val))
+			dir ^= 0x01;
+		OCR0A = PWM_val;
+		_delay_ms(10);
+		if (dir)
+		{
+			PWM_val++;
+		}
+		else
+		{
+			PWM_val--;
+		}
 	}
 
 	return 0;
