5b6f2ecb4a5b4f62ca58df235c1cf1d84ef9a3b6
mz-zajaczkowski <mz.zajaczkowski@gmail.com>
2017-02-13 22:42:13 +0100

    cleanup

--- testtiny85/main.c
+++ testtiny85/main.c
@@ -9,13 +9,6 @@
 #include <stdint.h>
 #include <math.h>
 
-static const double map_factor = 256/1024;
-
-double map_adc_to_pwm(uint8_t adcval)
-{
-	return map_factor * (1024 - adcval);
-}
-
 int main(void)
 {
 	DDRB |= (1 << PB0); // output on PB0
@@ -32,18 +25,12 @@ int main(void)
 	ADCSRA |= (1 << ADEN); // ADC enable
 	ADCSRA |= (1 << ADSC); // ADC start conversions
 
-	uint8_t PWM_val = 0u;
 	while(1)
 	{
-		if (ADCH < 255)
+		uint8_t reading = ADCH;
+		if (0u < reading && 255u > reading)
 		{
-		  double adc_mapped = map_adc_to_pwm(ADCH);
-		  PWM_val = floor(adc_mapped + 0.5);
-		  if (0u > PWM_val)
-		  	PWM_val = 0u;
-		  else if (255u < PWM_val)
-		  	PWM_val = 255u;
-		  OCR0A = PWM_val;
+		  OCR0A = reading;
 		  _delay_ms(10);
 		}
 	}
