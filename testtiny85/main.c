bbf654c4afe95a007e7ff64965307b8f6a9eb7dc
mizo <mizo@MIZO-PC>
2017-02-14 15:04:27 +0100

    Final working version of PWM set by pot value read with ADC

--- testtiny85/main.c
+++ testtiny85/main.c
@@ -9,30 +9,42 @@
 #include <stdint.h>
 #include <math.h>
 
+
 int main(void)
 {
 	DDRB |= (1 << PB0); // output on PB0
 	TCCR0A = (1 << COM0A1) | (1 << WGM00); //Phase-correct PWM
 	TCCR0B = (1 << CS01); // clock with prescaler CLK/8
 
-	ADCSRA |= (0 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // prescaler = 64
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (0 << ADPS0); // prescaler = 64
 
-	ADMUX  |= (1 << REFS0); // AVCC as reference voltage
+	ADMUX  |= (0 << REFS1) | (0 << REFS0); // AVCC as reference voltage
 	ADMUX  |= (1 << ADLAR); // left adjust ADC conversion result, now we need only ADCH to be read
 	ADMUX  |= (0 << MUX3) | (0 << MUX2) | (1 << MUX1) | (0 << MUX0); // use ADC2 for input (PB4)
 
-//	ADCSRA |= (1 << ADFR); // free-running mode
+
 	ADCSRA |= (1 << ADEN); // ADC enable
 	ADCSRA |= (1 << ADSC); // ADC start conversions
 
 	while(1)
 	{
-		uint8_t reading = ADCH;
-		if (0u < reading && 255u > reading)
+
+		if (ADCH < 0)
 		{
-		  OCR0A = reading;
-		  _delay_ms(10);
+			OCR0A = 0;
+			_delay_ms(10);
 		}
+		else if(ADCH > 255)
+		{
+			OCR0A = 255;
+			_delay_ms(10);
+		}
+		else
+		{
+			OCR0A = ADCH;
+			_delay_ms(10);
+		}
+		ADCSRA |= (1 << ADSC);
 	}
 
 	return 0;
